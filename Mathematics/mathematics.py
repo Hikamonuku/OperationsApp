dec14b3d56f208818d3670977fe7cf7a4c2df8f9
Hikamonuku <nogueirahodie@gmail.com>
2026-07-27 19:35:58 -0300

    Math subjects menu added om mathematics.py

--- Mathematics/mathematics.py
+++ Mathematics/mathematics.py
@@ -5,10 +5,10 @@ def select_math():
     while True:
         print("Math")
         print("Select the subarea you want to calculate: ")
-        math_subarea = input("1. Arithmetic 2. Back: ")
+        math_subarea = input("1. Arithmetic 2. Algebra 3. Geometry 4. Complex Numbers 5. Trigonometry 6. Back: ")
         if math_subarea == "1" or math_subarea.lower() == "arithmetic":
             arithmetic.select_arithmetics()
-        elif math_subarea == "2" or math_subarea.lower() == "back":
+        elif math_subarea == "6" or math_subarea.lower() == "back":
             return
         else:
             print("Invalid option")
